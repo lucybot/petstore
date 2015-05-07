37aa6cde457d6dd8ed3813d573f897ebfab1b24c
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-05-07 03:34:00 +0000

    Initialize

---
+++ server.js
@@ -0,0 +1,178 @@
+var FS = require('fs');
+var Hash = require('password-hash');
+var App = require('express')();
+var Jammin = require('jammin');
+App.use(require('cors')());
+
+module.exports.listen = function(port) {
+  console.log('listening: ' + port);
+  App.listen(port || 3000);
+}
+
+module.exports.dropAllEntries = function(callback) {
+  API.Pet.db.remove({}, function(err) {
+    if (err) throw err;
+    API.User.db.remove({}, function(err) {
+      if (err) throw err;
+      callback();
+    })
+  })
+}
+
+var DatabaseURL = JSON.parse(FS.readFileSync('./creds/mongo.json', 'utf8')).url;
+var API = new Jammin.API({
+  databaseURL: DatabaseURL,
+  swagger: {
+    info: {title: 'Pet Store', version: '0.1'},
+    host: 'api.example.com',
+    basePath: '/api',
+    securityDefinitions: {
+      username: { name: 'username', in: 'header', type: 'string'},
+      password: { name: 'password', in: 'header', type: 'string'}
+    },
+    definitions: {
+      User: {
+        properties: {
+          username: {type: 'string'},
+        }
+      },
+    }
+  }
+});
+
+var UserSchema = {
+  username: {type: String, required: true, unique: true, match: /^\w+$/},
+  password_hash: {type: String, required: true, select: false}
+}
+
+var PetSchema = {
+  id: {type: Number, required: true, unique: true},
+  name: String,
+  owner: String,
+  animalType: {type: String, default: 'unknown'},
+  vaccinations: [{
+    name: String,
+    date: Date
+  }]
+}
+
+var authenticateUser = function(req, res, next) {
+  var query = {
+    username: req.headers['username'],
+  };
+  API.User.db.findOne(query).select('+password_hash').exec(function(err, user) {
+    if (err) {
+      res.status(500).json({error: err.toString()})
+    } else if (!user || !user.password_hash) {
+      res.status(401).json({error: "Unknown user:" + query.username});
+    } else if (!Hash.verify(req.headers['password'], user.password_hash)) {
+      res.status(401).json({error: "Invalid password for " + query.username}) 
+    } else {
+      req.user = user;
+      next();
+    }
+  }) 
+}
+
+var SwaggerLogin = {
+  swagger: {
+    security: {username: [], password: []},
+  }
+}
+
+API.define('Pet', PetSchema);
+API.define('User', UserSchema);
+
+// Creates a new user.
+API.User.post('/users', {
+  swagger: {
+    parameters: [{
+        name: 'body',
+        in: 'body',
+        schema: {
+          properties: {
+            username: {type: 'string'},
+            password: {type: 'string'}
+          }
+      }
+    }]
+  }
+}, function(req, res, next) {
+  req.jammin.document.password_hash = Hash.generate(req.body.password);
+  next();
+});
+
+// Gets all users
+API.User.getMany('/users');
+
+// Gets a pet by id.
+API.Pet.get('/pets/:id');
+
+// Gets an array of pets that match the query.
+API.Pet.getMany('/pets');
+
+// Searches pets by name
+API.Pet.getMany('/search/pets', {
+  swagger: {
+    description: "Search all pets by name",
+    parameters: [
+      {name: 'q', in: 'query', type: 'string', description: 'Any regex'}
+    ]
+  }
+}, function(req, res, next) {
+  req.jammin.query = {
+    name: { "$regex": new RegExp(req.query.q) }
+  };
+  next();
+});
+
+var upsert = function(req, res, next) {
+  req.jammin.method = 'put';
+  next();
+}
+
+API.Pet.post('/pets/:id', SwaggerLogin, upsert, authenticateUser, function(req, res, next) {
+  req.jammin.document.owner = req.user.username;
+  req.jammin.document.id = req.params.id;
+  next();
+})
+
+// Creates one or more new pets.
+API.Pet.postMany('/pets', SwaggerLogin, authenticateUser, function(req, res, next) {
+  if (!Array.isArray(req.jammin.document)) req.jammin.document = [req.jammin.document];
+  req.jammin.document.forEach(function(pet) {
+    pet.owner = req.user.username;
+  });
+  next();
+});
+
+// Setting req.jammin.query.owner ensures that only the logged-in user's
+// pets will be returned by any queries Jammin makes to the DB.
+var enforceOwnership = function(req, res, next) {
+  req.jammin.query.owner = req.user.username;
+  next();
+}
+
+// Changes a pet.
+API.Pet.patch('/pets/:id', SwaggerLogin, authenticateUser, enforceOwnership);
+
+// Changes every pet that matches the query.
+API.Pet.patchMany('/pets', SwaggerLogin, authenticateUser, enforceOwnership);
+
+// Deletes a pet by ID.
+API.Pet.delete('/pets/:id', SwaggerLogin, authenticateUser, enforceOwnership);
+
+// Deletes every pet that matches the query.
+API.Pet.deleteMany('/pets', SwaggerLogin, authenticateUser, enforceOwnership);
+
+API.router.get('/pet_count', function(req, res) {
+  API.Pet.getMany(req, res, function(pets) {
+    res.json({count: pets.length})
+  })
+})
+
+API.Pet.getMany('/pet_types', {mapItem: function(pet) {return pet.animalType}})
+
+API.swagger('/swagger.json');
+
+App.use('/api', API.router);
---
+++ test/test.js
@@ -0,0 +1,317 @@
+var FS = require('fs');
+var Request = require('request');
+var Expect = require('chai').expect;
+var Petstore = require('../server.js');
+
+var SWAGGER_GOLDEN_FILE = __dirname + '/golden/petstore.swagger.json';
+var BASE_URL = 'http://127.0.0.1:3000/api';
+
+var USER_1 = {username: 'user1', password: 'jabberwocky'}
+var USER_2 = {username: 'user2', password: 'cabbagesandkings'}
+
+var PETS = [
+  {id: 0, name: "Pet0", owner: USER_1.username, animalType: "dog"},
+  {id: 1, name: "Pet1", owner: USER_1.username, animalType: "gerbil"},
+  {id: 2, name: "Pet2", owner: USER_2.username, animalType: "dog"},
+  {id: 3, name: "Pet3", owner: USER_1.username, animalType: "cat"},
+  {id: 4, name: "Pet4", owner: USER_1.username, animalType: "cat"},
+  {id: 5, name: "Pet5", owner: USER_1.username, animalType: "unknown"}
+];
+PETS.forEach(function(pet) {
+  pet.vaccinations = [];
+});
+
+var VACCINATIONS = [{name: 'Rabies', date: '1987-09-23T00:00:00.000Z'}];
+
+var CurrentPets = [];
+
+var successResponse = function(expectedBody, done) {
+  if (!done) {
+    done = expectedBody;
+    expectedBody = {success: true};
+  }
+  return function(err, res, body) {
+    if (body.error) console.log(body.error);
+    Expect(err).to.equal(null);
+    Expect(res.statusCode).to.equal(200);
+    Expect(body.error).to.equal(undefined);
+    if (!expectedBody) {
+      Expect(body.success).to.equal(true);
+    } else {
+      if (Array.isArray(expectedBody)) {
+        Expect(body).to.deep.have.members(expectedBody);
+        Expect(expectedBody).to.deep.have.members(body);
+      } else {
+        Expect(body).to.deep.equal(expectedBody);
+      }
+    }
+    done();
+  }
+}
+
+var failResponse = function(statusCode, done) {
+  return function(err, res, body) {
+    Expect(err).to.equal(null);
+    Expect(res.statusCode).to.equal(statusCode);
+    Expect(body.error).to.not.equal(null);
+    done();
+  }
+}
+
+describe('Petstore', function() {
+  before(function(done) {
+    Petstore.listen(3000);
+    Petstore.dropAllEntries(done);
+  });
+
+  it('should allow new users', function(done) {
+    Request.post({
+      url: BASE_URL + '/users',
+      body: USER_1,
+      json: true
+    }, successResponse(null, done));
+  });
+
+  it('should not show password_hash', function(done) {
+    Request.get({
+      url: BASE_URL + '/users',
+      json: true,
+    }, successResponse([{username: USER_1.username}], done))
+  });
+
+  it('should not allow duplicate user names', function(done) {
+    Request.post({
+      url: BASE_URL + '/users',
+      body: USER_1,
+      json: true
+    }, failResponse(500, done));
+  })
+
+  it('should allow a second user', function(done) {
+    Request.post({
+      url: BASE_URL + '/users',
+      body: USER_2,
+      json: true
+    }, successResponse(null, done));
+  })
+
+  it('should allow new pets', function(done) {
+    CurrentPets.push(PETS[1]);
+    Request.post({
+      url: BASE_URL + '/pets',
+      body: {id: 1, name: PETS[1].name, animalType: PETS[1].animalType},
+      headers: USER_1,
+      json: true
+    }, successResponse(null, done));
+  })
+
+  it('should allow a second pet', function(done) {
+    CurrentPets.push(PETS[2]);
+    Request.post({
+      url: BASE_URL + '/pets',
+      body: {id: 2, name: PETS[2].name, animalType: PETS[2].animalType},
+      headers: USER_2,
+      json: true
+    }, successResponse(null, done));
+  })
+
+  it('should upsert on post', function(done) {
+    PETS[2].name = 'Goose';
+    Request.post({
+      url: BASE_URL + '/pets/' + PETS[2].id,
+      body: PETS[2],
+      headers: USER_2,
+      json: true
+    }, successResponse(null, done))
+  })
+
+  it('should not allow modifications from wrong user', function(done) {
+    Request({
+      method: 'patch',
+      url: BASE_URL + '/pets/1',
+      headers: USER_2,
+      json: true
+    }, failResponse(404, done)); // TODO: should be 401
+  })
+
+  it('should allow modifications to pet', function(done) {
+    PETS[1].name = 'Loosey';
+    Request({
+      method: 'patch',
+      url: BASE_URL + '/pets/1',
+      headers: USER_1,
+      body: {name: PETS[1].name},
+      json: true
+    }, successResponse(done));
+  })
+
+  it('should allow searching', function(done) {
+    Request.get({
+      url: BASE_URL + '/search/pets',
+      qs: {q: 'oose'},
+      json: true
+    }, successResponse([PETS[1], PETS[2]], done))
+  })
+
+  it('should not allow duplicate pets', function(done) {
+    Request.post({
+      url: BASE_URL + '/pets',
+      body: {id: 1, name: 'Goose'},
+      headers: USER_1,
+      json: true
+    }, failResponse(500, done))
+  })
+
+  it('should not allow new pets without auth', function(done) {
+    Request.post({
+      url: BASE_URL + '/pets',
+      body: {id: 55, name: 'Goose'},
+      json: true
+    }, failResponse(401, done));
+  })
+
+  it('should not allow deletes without auth', function(done) {
+    Request({
+      method: 'delete',
+      url: BASE_URL + '/pets/1',
+      json: true
+    }, failResponse(401, done));
+  })
+
+  it('should not allow deletes from wrong user', function(done) {
+    Request({
+      method: 'delete',
+      url: BASE_URL + '/pets/1',
+      headers: USER_2,
+      json: true
+    }, failResponse(404, done)); // TODO: should return 401
+  });
+
+  it('should not reflect bad delete', function(done) {
+    Request.get({
+      url: BASE_URL + '/pets/1',
+      json: true
+    }, successResponse(PETS[1], done));
+  })
+
+  it('should not allow deletes with wrong password', function(done) {
+    Request({
+      method: 'delete',
+      url: BASE_URL + '/pets/1',
+      headers: {
+        username: USER_1.username,
+        password: USER_2.password
+      },
+      json: true
+    }, failResponse(401, done));
+  })
+
+  it('should allow deletes from owner', function(done) {
+    CurrentPets.shift();
+    Request({
+      method: 'delete',
+      url: BASE_URL + '/pets/1',
+      headers: USER_1,
+      json: true
+    }, successResponse(null, done));
+  })
+
+  it('should allow batched adds of pets', function(done) {
+    CurrentPets.unshift(PETS[3], PETS[4], PETS[5]);
+    Request.post({
+      url: BASE_URL + '/pets',
+      headers: USER_1,
+      body: [PETS[3], PETS[4], PETS[5]],
+      json: true
+    }, successResponse(null, done))
+  })
+
+  it('should allow batched modification of pets', function(done) {
+    CurrentPets.forEach(function(pet) {
+      if (pet.owner == USER_1.username && pet.animalType === 'cat') pet.animalType = 'dog';
+    });
+    Request({
+      method: 'patch',
+      url: BASE_URL + '/pets',
+      headers: USER_1,
+      qs: {
+        animalType: 'cat'
+      },
+      body: {
+        animalType: 'dog'
+      },
+      json: true
+    }, successResponse(null, done))
+  })
+
+  it('should reflect batched modification', function(done) {
+    Request.get({
+      url: BASE_URL + '/pets',
+      qs: {
+        animalType: 'dog'
+      },
+      json: true
+    }, successResponse(CurrentPets.filter(function(pet) {return pet.animalType === 'dog'}), done))
+  })
+
+  it('should support pet_count', function(done) {
+    Request.get({
+      url: BASE_URL + '/pet_count',
+      json: true
+    }, successResponse({count: CurrentPets.length}, done));
+  });
+
+  it('should support mapItem to get pet types', function(done) {
+    Request.get({
+      url: BASE_URL + '/pet_types',
+      json: true,
+    }, successResponse(CurrentPets.map(function(pet) {return pet.animalType}), done));
+  })
+
+  it('should allow adding a vaccination', function(done) {
+    PETS[3].vaccinations = VACCINATIONS;
+    Request.patch({
+      url: BASE_URL + '/pets/3',
+      headers: USER_1,
+      body: {vaccinations: VACCINATIONS},
+      json: true,
+    }, successResponse(done))
+  })
+
+  it('should reflect added vaccination', function(done) {
+    Request.get({
+      url: BASE_URL + '/pets/3',
+      json: true,
+    }, successResponse(PETS[3], done));
+  })
+
+  it('should allow batched deletes of pets', function(done) {
+    CurrentPets = CurrentPets.filter(function(pet) {return pet.animalType !== 'dog'});
+    Request({
+      method: 'delete',
+      url: BASE_URL + '/pets',
+      headers: USER_1,
+      qs: {
+        animalType: "dog"
+      },
+      json: true
+    }, successResponse(null, done))
+  })
+
+  it('should serve swagger docs', function(done) {
+    Request.get({
+      url: BASE_URL + '/swagger.json',
+      json: true
+    }, function(err, res, body) {
+      Expect(err).to.equal(null);
+      if (process.env.WRITE_GOLDEN) {
+        console.log("Writing new golden file!");
+        FS.writeFileSync(SWAGGER_GOLDEN_FILE, JSON.stringify(body, null, 2));
+      } else {
+        var golden = JSON.parse(FS.readFileSync(SWAGGER_GOLDEN_FILE, 'utf8'));
+        Expect(body).to.deep.equal(golden);
+      }
+      done();
+    })
+  })
+})
